fef81194a44ae66b6cd2b3c1a20ad5787198e03b
diqn <diqqqn89@email.com>
2022-06-18 04:00:09 +0300

    add checkout and fix findroom

--- Hotel.java
+++ Hotel.java
@@ -1,15 +1,15 @@
 import java.util.Scanner;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 
 public class Hotel {
     static Scanner sc;
     static final String datePattern = "(0?[1-9]|[12][0-9]|3[01]).(0?[1-9]|1[012]).((?:19|20)[0-9][0-9])";
     static Pattern pattern = Pattern.compile(datePattern);
-    static Matcher sDateMatcher;
-    static Matcher eDateMatcher;
-    static String[] rooms = { "101", "102", "103", "104", "105", "106", "201", "202", "203", "204", "205",
-            "301", "302", "303", "304", "401", "402", "403", "501", "502", "601" };
+    static String[] rooms = { "101", "102", "103", "104", "201", "202", "203",
+            "301", "302", "303", "401", "402", "501", "502", "601" };
+    static int[] roomsBeds = { 1, 1, 1, 1, 2, 2, 2, 3, 3, 3, 3, 3, 4, 4, 5 };
     static String[] startDate = new String[rooms.length];
     static String[] endDate = new String[rooms.length];
     static String[] comment = new String[rooms.length];
@@ -40,7 +40,87 @@ public class Hotel {
         }
     }
 
-    private static void updateRoom() {
+    public static boolean getDateRange(String startDate, String endDate, String checkDate) {
+        String[] checkDateArr;
+        String[] checkStartDateArr;
+        String[] checkEndDateArr;
+
+        if (checkDate == null) {
+            return false;
+        } else {
+            checkDateArr = checkDate.split("\\.");
+        }
+        if (startDate == null) {
+            return false;
+        } else {
+            checkStartDateArr = startDate.split("\\.");
+        }
+        if (endDate == null) {
+            return false;
+        } else {
+            checkEndDateArr = endDate.split("\\.");
+        }
+
+        int checkDateYear = Integer.parseInt(checkDateArr[2]);
+        int checkDateMonth = Integer.parseInt(checkDateArr[1]);
+        int checkDateday = Integer.parseInt(checkDateArr[0]);
+        LocalDate myDate = LocalDate.of(checkDateYear, checkDateMonth, checkDateday);
+
+        int startDateYear = Integer.parseInt(checkStartDateArr[2]);
+        int startDateMonth = Integer.parseInt(checkStartDateArr[1]);
+        int startDateday = Integer.parseInt(checkStartDateArr[0]);
+        LocalDate start = LocalDate.of(startDateYear, startDateMonth, startDateday);
+
+        int endDateYear = Integer.parseInt(checkEndDateArr[2]);
+        int endDateMonth = Integer.parseInt(checkEndDateArr[1]);
+        int endDateday = Integer.parseInt(checkEndDateArr[0]);
+        LocalDate stop = LocalDate.of(endDateYear, endDateMonth, endDateday);
+
+        Boolean containsToday = (!myDate.isBefore(start)) && (!myDate.isBefore(stop));
+        return containsToday;
+    }
+
+    public static boolean getDateRangeForStatus(String startDate, String endDate, String checkDate) {
+        String[] checkDateArr;
+        String[] checkStartDateArr;
+        String[] checkEndDateArr;
+
+        if (checkDate == null) {
+            return false;
+        } else {
+            checkDateArr = checkDate.split("\\.");
+        }
+        if (startDate == null) {
+            return false;
+        } else {
+            checkStartDateArr = startDate.split("\\.");
+        }
+        if (endDate == null) {
+            return false;
+        } else {
+            checkEndDateArr = endDate.split("\\.");
+        }
+
+        int checkDateYear = Integer.parseInt(checkDateArr[2]);
+        int checkDateMonth = Integer.parseInt(checkDateArr[1]);
+        int checkDateday = Integer.parseInt(checkDateArr[0]);
+        LocalDate myDate = LocalDate.of(checkDateYear, checkDateMonth, checkDateday);
+
+        int startDateYear = Integer.parseInt(checkStartDateArr[2]);
+        int startDateMonth = Integer.parseInt(checkStartDateArr[1]);
+        int startDateday = Integer.parseInt(checkStartDateArr[0]);
+        LocalDate start = LocalDate.of(startDateYear, startDateMonth, startDateday);
+
+        int endDateYear = Integer.parseInt(checkEndDateArr[2]);
+        int endDateMonth = Integer.parseInt(checkEndDateArr[1]);
+        int endDateday = Integer.parseInt(checkEndDateArr[0]);
+        LocalDate stop = LocalDate.of(endDateYear, endDateMonth, endDateday);
+
+        Boolean containsToday = (!myDate.isBefore(start)) && (myDate.isBefore(stop));
+        return containsToday;
+    }
+
+    public static void updateRoom() {
         sc = new Scanner(System.in);
         System.out.print("Enter room number: ");
         String roomNum = sc.nextLine();
@@ -81,7 +161,10 @@ public class Hotel {
 
             sDateArr = sDate.split("\\.");
             eDateArr = eDate.split("\\.");
-
+            if (sDateArr.length <= 2 && eDateArr.length <= 2) {
+                System.out.println("Invalid Date:");
+                return;
+            }
             if (Integer.parseInt(sDateArr[1]) > Integer.parseInt(eDateArr[1]) ||
                     Integer.parseInt(sDateArr[1]) >= Integer.parseInt(eDateArr[1])
                             && Integer.parseInt(sDateArr[0]) > Integer.parseInt(eDateArr[0])
@@ -99,7 +182,7 @@ public class Hotel {
         endDate[roomNumIndex] = eDate;
 
         do {
-            System.out.println("Your old note is : '" + comment[roomNumIndex] + "'");
+            System.out.println("Your current note is : '" + comment[roomNumIndex] + "'");
             System.out.print("Enter your new note: ");
             comment[roomNumIndex] = sc.nextLine();
             if (comment[roomNumIndex] == "" || comment[roomNumIndex] == null) {
@@ -109,16 +192,20 @@ public class Hotel {
 
     }
 
-    private static void findRoom() {
-    }
-
-    private static void status() {
+    public static void findRoom() {
         sc = new Scanner(System.in);
+
         String sDateArr[];
         String eDateArr[];
         String sDate;
         String eDate;
+        int numberOfBed = -1;
 
+        do {
+            System.out.print("Enter number of beds: ");
+            numberOfBed = sc.nextInt();
+        } while (numberOfBed <= 0 && numberOfBed >= 5);
+        sc = new Scanner(System.in);
         do {
             System.out.print("Start date: ");
             sDate = sc.nextLine();
@@ -145,22 +232,23 @@ public class Hotel {
                 || Integer.parseInt(sDateArr[1]) >= Integer.parseInt(eDateArr[1])
                         && Integer.parseInt(sDateArr[2]) > Integer.parseInt(eDateArr[2]));
 
-        for (int i = 0; i < rooms.length; i++) {
-            if (startDate[i] != null && endDate[i] != null && comment[i] != null) {
-                String[] currentRoomStartDateArr = startDate[i].split("\\.");
-                String[] currentRoomEndDateArr = startDate[i].split("\\.");
-                // TODO: nedovur6ena proverka za dati
-                if (Integer.parseInt(currentRoomStartDateArr[0]) >= Integer.parseInt(sDateArr[0])
-                        && Integer.parseInt(currentRoomEndDateArr[0]) <= Integer.parseInt(eDateArr[0])) {
+        for (int ind = 0; ind < rooms.length; ind++) {
+            if (roomsBeds[ind] == numberOfBed) {
 
-                    System.out.println(rooms[i] + ": "
-                            + (Integer.parseInt(eDateArr[0]) - Integer.parseInt(currentRoomStartDateArr[0]) + " days"));
+                if (comment[ind] == null && startDate[ind] == null && endDate[ind] == null) {
+                    System.out.println(rooms[ind]);
+                } else {
+                    if (getDateRange(sDate, eDate, startDate[ind]) && getDateRange(sDate, eDate, endDate[ind])) {
+                        System.out.println(rooms[ind]);
+                    }
                 }
+
             }
         }
+
     }
 
-    private static void checkoutRoom() {
+    public static void status() {
         sc = new Scanner(System.in);
         String sDateArr[];
         String eDateArr[];
@@ -179,7 +267,10 @@ public class Hotel {
 
             sDateArr = sDate.split("\\.");
             eDateArr = eDate.split("\\.");
-
+            if (sDateArr.length <= 2 && eDateArr.length <= 2) {
+                System.out.println("Invalid Date:");
+                return;
+            }
             if (Integer.parseInt(sDateArr[1]) > Integer.parseInt(eDateArr[1]) ||
                     Integer.parseInt(sDateArr[1]) >= Integer.parseInt(eDateArr[1])
                             && Integer.parseInt(sDateArr[0]) > Integer.parseInt(eDateArr[0])
@@ -192,25 +283,51 @@ public class Hotel {
                         && Integer.parseInt(sDateArr[0]) > Integer.parseInt(eDateArr[0])
                 || Integer.parseInt(sDateArr[1]) >= Integer.parseInt(eDateArr[1])
                         && Integer.parseInt(sDateArr[2]) > Integer.parseInt(eDateArr[2]));
-
+        SimpleDateFormat formater = new SimpleDateFormat("dd.MM.yyyy");
+        long d1;
+        long d2;
+        long total = 0;
         for (int i = 0; i < rooms.length; i++) {
-            if (startDate[i] == null && endDate[i] == null) {
-                System.out.println("Available rooms:" + rooms[i]);
-            }
-            if (startDate[i] != null && endDate[i] != null) {
-                String[] roomStartArr = startDate[i].split("\\.");
-                String[] roomEndArr = endDate[i].split("\\.");
-                // TODO:trqbva da se fixne proverkata za intervala na zaetata staq
-                if (Integer.parseInt(roomStartArr[0]) >= Integer.parseInt(sDateArr[0])
-                        && Integer.parseInt(roomEndArr[0]) <= Integer.parseInt(eDateArr[0])) {
-                    System.out.println("Available rooms:" + rooms[i]);
+            if (comment[i] == null && startDate[i] == null && endDate[i] == null) {
+                System.out.println(rooms[i] + ": 0 days");
+            } else {
+                if (getDateRangeForStatus(sDate, eDate, startDate[i])
+                        || getDateRangeForStatus(sDate, eDate, endDate[i])) {
+
+                    System.out.println(rooms[i]);
                 }
             }
-
         }
     }
 
-    private static void listFreeRooms() {
+    public static void checkoutRoom() {
+        sc = new Scanner(System.in);
+        System.out.print("Enter room num for checkout: ");
+        String checkoutRoomNumber = sc.nextLine();
+        int checkoutRoomIndex = -1;
+
+        for (int i = 0; i < rooms.length; i++) {
+            if (rooms[i].equals(checkoutRoomNumber)) {
+                checkoutRoomIndex = rooms[i].indexOf(checkoutRoomNumber);
+            }
+        }
+        if (checkoutRoomIndex != -1) {
+            if (startDate[checkoutRoomIndex] != null && endDate[checkoutRoomIndex] != null
+                    && comment[checkoutRoomIndex] != null) {
+                startDate[checkoutRoomIndex] = null;
+                endDate[checkoutRoomIndex] = null;
+                comment[checkoutRoomIndex] = null;
+                System.out.println(rooms[checkoutRoomIndex] + "[+] room now is free !");
+            } else {
+                System.out.println("Room is free");
+            }
+        } else {
+            System.out.println("Invalid room number");
+        }
+
+    }
+
+    public static void listFreeRooms() {
         for (int i = 0; i < rooms.length; i++) {
             if (startDate[i] == null && endDate[i] == null && comment[i] == null) {
                 System.out.println("Free room - " + rooms[i]);
@@ -218,7 +335,7 @@ public class Hotel {
         }
     }
 
-    private static void makeReservation() {
+    public static void makeReservation() {
         sc = new Scanner(System.in);
         System.out.print("Enter room number: ");
         String roomNum = sc.nextLine();
@@ -245,6 +362,10 @@ public class Hotel {
                 eDate = sc.nextLine();
                 sDateArr = sDate.split("\\.");
                 eDateArr = eDate.split("\\.");
+                if (sDateArr.length <= 2 && eDateArr.length <= 2) {
+                    System.out.println("Invalid Date:");
+                    return;
+                }
                 if (Integer.parseInt(sDateArr[1]) > Integer.parseInt(eDateArr[1])
                         || Integer.parseInt(sDateArr[2]) > Integer.parseInt(eDateArr[2])
                         || Integer.parseInt(sDateArr[0]) >= Integer.parseInt(eDateArr[0])
@@ -278,6 +399,17 @@ public class Hotel {
     }
 
     public static void myHotel() {
+        // SimpleDateFormat formater = new SimpleDateFormat("dd.MM.yyyy");
+        // long d1;
+        // long d2;
+        // try {
+        // d1 = formater.parse("1.1.2001").getTime();
+        // d2 = formater.parse("1.1.2002").getTime();
+        // System.out.println(Math.abs((d1 - d2) / (1000 * 60 * 60 * 24)));
+        // } catch (ParseException e) {
+        // e.printStackTrace();
+        // }
+
         sc = new Scanner(System.in);
         int choice;
 
